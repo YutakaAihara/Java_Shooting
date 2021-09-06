ec8a45b7b95c0695577dbb4417f0fc781c905663
YutakaAihara <youguisufanyuan@gmail.com>
2021-09-06 11:35:23 +0900

    描画の設定

--- src/Java_Shooting/Shooting.java
+++ src/Java_Shooting/Shooting.java
@@ -1,9 +1,37 @@
 package Java_Shooting;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Shooting {
 	public static ShootingFrame shootingFrame;
+	public static boolean loop;
 	
 	public static void main(String[] args) {
 		shootingFrame = new ShootingFrame();
+		loop = true;
+		
+		Graphics gra = shootingFrame.panel.image.getGraphics();
+		
+		//FPS
+		long startTime;
+		int fps = 30;
+		while(loop) {
+			startTime = System.currentTimeMillis();
+			
+			gra.setColor(Color.WHITE);
+			gra.fillRect(0,  0,  500,  500);
+			
+			gra.setColor(Color.BLACK);
+			gra.fillRect(100,  100,  100, 100);
+			
+			
+			try {
+				Thread.sleep((1000 / fps) - (System.currentTimeMillis() - startTime));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
 	}
 }
