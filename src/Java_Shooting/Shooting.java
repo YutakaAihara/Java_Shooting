d01f0512a61da96dba084cb2470756e7947d7edf
YutakaAihara <youguisufanyuan@gmail.com>
2021-09-06 11:55:18 +0900

    FPS描画

--- src/Java_Shooting/Shooting.java
+++ src/Java_Shooting/Shooting.java
@@ -1,6 +1,7 @@
 package Java_Shooting;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 public class Shooting {
@@ -15,8 +16,18 @@ public class Shooting {
 		
 		//FPS
 		long startTime;
+		long fpsTime = 0;
 		int fps = 30;
+		int FPS = 0;
+		int FPSCount = 0;
 		while(loop) {
+			if((System.currentTimeMillis() - fpsTime) > 1000 ) {
+				fpsTime = System.currentTimeMillis();
+				FPS = FPSCount;
+				FPSCount = 0;
+				System.out.println(FPS);
+			}
+			FPSCount++;
 			startTime = System.currentTimeMillis();
 			
 			gra.setColor(Color.WHITE);
@@ -25,13 +36,21 @@ public class Shooting {
 			gra.setColor(Color.BLACK);
 			gra.fillRect(100,  100,  100, 100);
 			
+			gra.setColor(Color.BLACK);
+			gra.setFont(new Font("SanSerif", Font.PLAIN, 20));
+			gra.drawString(FPS + "FPS", 0, 400);
+			
+			shootingFrame.panel.draw();
+			
 			
 			try {
-				Thread.sleep((1000 / fps) - (System.currentTimeMillis() - startTime));
+				long runTime = System.currentTimeMillis() - startTime;
+				if(runTime<(1000/fps)) {
+					Thread.sleep((1000 / fps) - (runTime));				
+				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
 		}
 	}
 }
